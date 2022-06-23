cfd726fd4dcc5ed0cd083dd266252e710f64efd5
Sanmi01 <supersanmi01@gmail.com>
2022-06-23 22:37:49 +0100

    made changes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.write("<p>Name: Oluwasanmi Awelewa <br> Height: 5'9 <br> Country: Nigeria</p>");
-alert(" Name: Oluwasanmi Awelewa \n Height: 5'9 \n Country: Nigeria" )
-console.log((" Name: Oluwasanmi Awelewa \n Height: 5'9 \n Country: Nigeria" ))
+document.write("<p>Name: Oluwasanmi Awelewa <br> Height: 5 feet 9 inches <br> Country: Nigeria</p>");
+alert(" Name: Oluwasanmi Awelewa \n Height: 5 feet 9 inches \n Country: Nigeria" )
+console.log((" Name: Oluwasanmi Awelewa \n Height: 5 feet 9 inches \n Country: Nigeria" ))
